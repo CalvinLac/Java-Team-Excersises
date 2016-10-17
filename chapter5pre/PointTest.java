b784e7705fd35644ed0d4617f4ed0be13ec8447d
Calvin <Calvin.lacc@gmail.com>
2016-10-17 00:08:46 -0600

    Adding euclidean distance between points

--- chapter5pre/PointTest.java
+++ chapter5pre/PointTest.java
@@ -2,8 +2,9 @@ public class PointTest {
 
   public static void main(String[] args) { 
 
-    Point hi = new Point(5,5);
-    hi.moveLeft(6);
+    Point hi = new Point(3,4);
+    Point bye = new Point(0,0);
+    bye.distance(hi);
 
     System.out.println("The X coordinate is " + hi.getX() + " and the Y coordinate is " + hi.getY());
 
